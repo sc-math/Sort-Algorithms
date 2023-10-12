67092c92722c89777db2bc7507348dd6a58936ee
Matheus Silva <scruz.math@gmail.com>
2023-10-12 03:41:20 -0300

    Update Bubble Sort

--- Bubble Sort/bubble_sort.dart
+++ Bubble Sort/bubble_sort.dart
@@ -1,23 +1,25 @@
 BubbleSort(List<int> list){
 
   for(int i = list.length; i > 0; i--){
-
-    bool isChanged = false;
-
-    for(int j = 1; j < list.length; j++){
+    bool swapped = false;
+    for(int j = 1; j < i; j++){
 
       if(list[j-1] > list[j]){
 
+        /*
+        * Realiza a troca entre os valores adjacentes
+        * deixando o maior valor sempre na direita
+        * */
         int t = list[j-1];
         list[j-1] = list[j];
         list[j] = t;
 
-        isChanged = true;
+        swapped = true;
       }
     }
 
-    if(!isChanged){
-      return;
-    }
+    // Se não ouve trocar, o Arr[i ... n] está ordenado
+    if(!swapped)
+      break;
   }
 }
