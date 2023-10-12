65e05b0d64f6f4ed12af5576191ddb8804982901
Matheus Silva <scruz.math@gmail.com>
2023-10-12 03:18:44 -0300

    Add comments

--- Selection Sort/selection_sort.dart
+++ Selection Sort/selection_sort.dart
@@ -2,8 +2,13 @@ SelectionSort(List<int> list){
 
   int min;
 
+  // Percorre a parte do Vetor não Ordenado
   for(int i = 0; i < list.length-1; i++){
 
+    /*
+    * Procura o menor elemento no intervalo
+    * não ordenado do Vetor
+    * */
     min = i;
 
     for(int j = i+1; j < list.length; j++){
@@ -13,6 +18,11 @@ SelectionSort(List<int> list){
       }
     }
 
+    /*
+    * Coloca o elemento de menor valor do intervalo
+    * não ordenado do Vetor na posição i
+    * (posição correta dele no vetor ordenado)
+    * */
     int t = list[min];
     list[min] = list[i];
     list[i] = t;
